e97c8e3c97aa68f9a05d9ac16587bb78610fb638
Ran <xyfwdy@gmail.com>
2026-07-19 07:47:42 +0800

    优化 清理冗余的导入语句 优化 删除多余的插件实例变量

--- src/main/java/cc/ranmc/farm/papi/RanmcPapi.java
+++ src/main/java/cc/ranmc/farm/papi/RanmcPapi.java
@@ -1,16 +1,11 @@
 package cc.ranmc.farm.papi;
 
-import cc.ranmc.farm.Main;
 import cc.ranmc.farm.bean.SQLRow;
-import cc.ranmc.farm.constant.SQLKey;
-import cc.ranmc.farm.bean.SQLFilter;
 import cc.ranmc.farm.util.DataUtil;
 import cc.ranmc.papi.bean.PapiHandler;
 import org.bukkit.entity.Player;
 
 public class RanmcPapi extends PapiHandler {
-
-    private final Main plugin = Main.getInstance();
 
     public RanmcPapi(String prefix) {
         super(prefix);
