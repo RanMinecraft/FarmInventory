b977de16a6ccd46e7275d06948b0d6a48a547762
Ran <xyfwdy@gmail.com>
2026-07-19 08:15:01 +0800

    修复 错误

--- src/main/java/cc/ranmc/farm/sql/Database.java
+++ src/main/java/cc/ranmc/farm/sql/Database.java
@@ -14,7 +14,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-import static cc.ranmc.utils.BasicUtil.print;
+import static cc.ranmc.farm.util.FarmUtil.print;
 
 public class Database {
 
